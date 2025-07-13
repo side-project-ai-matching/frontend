1fe2375f69380a9cee7beb7289abff94b424aa32
OseungKwon <00osos@naver.com>
2025-07-13 22:40:47 +0900

    feat: login router 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./features/auth/pages/RegisterPage";
+import LoginPage from "./features/auth/pages/LoginPage";
+import { Header } from "./widgets/Header";
+import { Body } from "./widgets/Body";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<div>메인 페이지</div>} />
-      </Routes>
+      <Header />
+      <Body>
+        <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<div>메인 페이지</div>} />
+        </Routes>
+      </Body>
     </BrowserRouter>
   );
 }
